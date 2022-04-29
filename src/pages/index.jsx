6f18e15764c27ad24a1aaf27855ed1e7bb444e27
stevesscb <stevesscb@gmail.com>
2022-04-29 10:50:41 +0800

    added loading skeletons to index pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,18 +3,17 @@
 import React from 'react'
 import Head from 'next/head'
 import Container from 'react-bootstrap/Container'
-import { Card, Col, Row, Carousel } from 'react-bootstrap'
+import { Card, Col, Row } from 'react-bootstrap'
 import Link from 'next/link'
 import Skeleton from 'react-loading-skeleton'
 
 import useMusicians from '@/hooks/musicians'
 
 export default function Home() {
-  const { musicians } = useMusicians()
+  const { musicians, isLoading } = useMusicians()
 
   return (
     <div id="homePageContainer">
-
       <Head>
         <title>BAND-AID | HOME</title>
         <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
@@ -37,55 +36,59 @@ export default function Home() {
 
       <Container className="cards">
         <h6>Newest Members</h6>
+
         <Row xs={1} md={4} className="g-4">
           {
-            musicians.map((musician) => (
-              <Col key={musician.id} className="card-body">
-                <Link href={`/musicians/${musician.id}`} passHref>
-                  <Card className="grow h-100">
-                    <div className="card-image ratio ratio-4x3">
-                      <Card.Img
-                        className="w-100 h-100"
-                        style={{ objectFit: 'cover' }}
-                        variant="top"
-                        src={musician.portraits[0].file}
+            !isLoading ? (
+              musicians.map((musician) => (
+                <Col key={musician.id} className="card-body">
+                  <Link href={`/musicians/${musician.id}`} passHref>
+                    <Card className="grow h-100">
+                      <div className="card-image ratio ratio-4x3">
+                        <Card.Img
+                          className="w-100 h-100"
+                          style={{ objectFit: 'cover' }}
+                          variant="top"
+                          src={musician.portraits[0].file}
+                        />
+                      </div>
+                      <Card.Body>
+                        <Card.Title>{musician.displayName}</Card.Title>
+                        <Card.Text>
+                          <React.Fragment key={musician.id}>
+                            <dt>Instruments:</dt>{
+                              musician.instruments.map((instrument) => (
+                                <React.Fragment key={instrument.id}>
+                                  <dd>
+                                    {instrument.type}
+                                  </dd>
+                                </React.Fragment>
+                              ))
+                              }
+                            <dt>In a band?</dt>
+                            <dd>{musician.inBand.toString()}</dd>
+                          </React.Fragment>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+              ))
+            ) : (
+              Array(4).fill(null).map((x, i) => (
+                <Col key={i} className="card-body">
+                  <Card className="grow">
+                    <div className="card-img ratio ratio-4x3">
+                      <Skeleton
+                        height="100%"
                       />
                     </div>
                     <Card.Body>
-                      <Card.Title>{musician.displayName}</Card.Title>
-                      <Card.Text>
-                        <React.Fragment key={musician.id}>
-                          <dt>Instruments:</dt>{
-                            musician.instruments.map((instrument) => (
-                              <React.Fragment key={instrument.id}>
-                                <dd>
-                                  {instrument.type}
-                                </dd>
-                              </React.Fragment>
-                            ))
-                            }
-                          <dt>In a band?</dt>
-                          <dd>{musician.inBand.toString()}</dd>
-                        </React.Fragment>
-                      </Card.Text>
+                      <Skeleton count={8} />
                     </Card.Body>
                   </Card>
-                </Link>
-              </Col>
-            )) || (
-              <Col className="card-body">
-                <Card className="grow">
-                  <Card.Img>
-                    <Skeleton
-                      width={400}
-                      height={400}
-                    />
-                  </Card.Img>
-                  <Card.Body>
-                    <Skeleton count={8} />
-                  </Card.Body>
-                </Card>
-              </Col>
+                </Col>
+              ))
             )
           }
         </Row>
--- src/pages/musicians/index.jsx
+++ src/pages/musicians/index.jsx
@@ -9,7 +9,7 @@ import withAuth from '@/hoc/withAuth'
 import useMusicians from '@/hooks/musicians'
 
 export function PagesMusiciansIndex() {
-  const { musicians } = useMusicians()
+  const { musicians, isLoading } = useMusicians()
 
   return (
     <div id="musicians-container">
@@ -20,26 +20,28 @@ export function PagesMusiciansIndex() {
 
       <Container className="musicians-cards">
         <h4 className="p-3">All Musicians</h4>
+
         <Row xs={1} md={4} className="g-4">
           {
-            musicians.map((musician) => (
-              <Col key={musician.id} className="card-body">
-                <Link href={`/musicians/${musician.id}`} passHref>
-                  <Card className="grow h-100">
-                    <div className="card-image ratio ratio-4x3">
-                      <Card.Img
-                        className="w-100 h-100"
-                        style={{ objectFit: 'cover' }}
-                        variant="top"
-                        src={musician.portraits[0]?.file || <Skeleton height={400} width="80%" />}
-                      />
-                    </div>
-                    <Card.Body>
-                      <Card.Title>{musician.displayName || <Skeleton />}</Card.Title>
-                      <Card.Text>
-                        <React.Fragment key={musician.id}>
+            !isLoading ? (
+              musicians.map((musician) => (
+                <Col key={musician.id} className="card-body">
+                  <Link href={`/musicians/${musician.id}`} passHref>
+                    <Card className="grow h-100">
+                      <div className="card-image ratio ratio-4x3">
+                        <Card.Img
+                          className="w-100 h-100"
+                          style={{ objectFit: 'cover' }}
+                          variant="top"
+                          src={musician.portraits[0]?.file || <Skeleton height={400} width="80%" />}
+                        />
+                      </div>
+                      <Card.Body>
+                        <Card.Title>{musician.displayName || <Skeleton />}</Card.Title>
+                        <Card.Text>
+                          <React.Fragment key={musician.id}>
 
-                          <dt>Instruments:</dt>{
+                            <dt>Instruments:</dt>{
                             musician.instruments.map((instrument) => (
                               <React.Fragment key={instrument.id}>
                                 <dd>
@@ -48,29 +50,31 @@ export function PagesMusiciansIndex() {
                               </React.Fragment>
                             ))
                             }
-                          <dt>In a band?</dt>
-                          <dd>{musician.inBand.toString()}</dd>
+                            <dt>In a band?</dt>
+                            <dd>{musician.inBand.toString()}</dd>
 
-                        </React.Fragment>
-                      </Card.Text>
+                          </React.Fragment>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Link>
+                </Col>
+              ))
+            ) : (
+              Array(4).fill(null).map((x, i) => (
+                <Col key={i} className="card-body">
+                  <Card className="grow">
+                    <div className="card-img ratio ratio-4x3">
+                      <Skeleton
+                        height="100%"
+                      />
+                    </div>
+                    <Card.Body>
+                      <Skeleton count={8} />
                     </Card.Body>
                   </Card>
-                </Link>
-              </Col>
-            )) || (
-              <Col className="card-body h-100 w-50">
-                <Card className="grow" style={{ maxWidth: '400px', maxHeight: '800px' }}>
-                  <Card.Img>
-                    <Skeleton
-                      width={400}
-                      height={400}
-                    />
-                  </Card.Img>
-                  <Card.Body>
-                    <Skeleton count={8} />
-                  </Card.Body>
-                </Card>
-              </Col>
+                </Col>
+              ))
             )
           }
         </Row>
